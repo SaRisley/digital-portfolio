87ab3481b755bae564d3f1373fa8bc24be02d8d2
Sarah Risley <sarah.risley@hotmail.com>
2023-11-02 11:41:31 +0000

    move images folder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ const Header = () => {
 
     return(
         <ul className="header">
-            <li className="heading-content"><a href='https://www.linkedin.com/in/sarah-risley-0a5064163/'><img src='src/images/linkedIn-mark.png' className='img-header'></img></a></li>
-            <li className="heading-content"><a href='mailto: sarah.risley@hotmail.com'><img src='src/images/email.png' className='img-header'></img></a></li>
-            <li className="heading-content"><a href='https://github.com/SaRisley'><img src='src/images/github-mark.png' className='img-header'></img></a></li>
+            <li className="heading-content"><a href='https://www.linkedin.com/in/sarah-risley-0a5064163/'><img src='images/linkedIn-mark.png' alt='linkedin' className='img-header'></img></a></li>
+            <li className="heading-content"><a href='mailto: sarah.risley@hotmail.com'><img src='images/email.png' alt='email' className='img-header'></img></a></li>
+            <li className="heading-content"><a href='https://github.com/SaRisley'><img src='images/github-mark.png' alt='github' className='img-header'></img></a></li>
         </ul>
     );
 
